7f694b7ac827604318f1906fa04f75a41049d74c
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-07-20 13:43:16 -0500

    app.js

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const inquirer = require('inquirer');
 
 // const generatePage = require('./src/page-template')
 
-inquirer
-    .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
         }
     ])
-    .then(answers => console.log(answers));
-    
+};
+
+promptUser().then(answers => console.log(answers));
+
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
