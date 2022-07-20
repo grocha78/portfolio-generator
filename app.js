86f2f44d0198e6681b4aeb40026c03c6dc109d98
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-07-20 13:44:16 -0500

    app.js

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template')
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template')
 
-console.log(profileDataArgs);
+const promptUser = () => {
+  return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+};
 
-const [name, github] = profileDataArgs;
+promptUser().then(answers => console.log(answers));
 
-console.log(name, github);
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+// if (err) throw new Error(err);
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-if (err) throw new Error(err);
-
-console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+// console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
